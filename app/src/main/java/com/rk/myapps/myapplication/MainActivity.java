a73aef6e02c84ce7f948101a91fe26f166a71f94
riddhik84 <riddhik84@gmail.com>
2016-03-09 18:59:25 +0530

    Version 1: Sample ScrollView

--- app/src/main/java/com/rk/myapps/myapplication/MainActivity.java
+++ app/src/main/java/com/rk/myapps/myapplication/MainActivity.java
@@ -15,15 +15,10 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    //@Bind(R.id.image_test1)
-    //ImageView image1;
-
-    //@Bind(R.id.image_test2)
-    //ImageView image2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,28 +52,23 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.showImagesExample)
     public void showImage(View view) {
+        //Intent intent = new Intent(getApplicationContext(), PicassoImageView.class);
+        //startActivity(intent);
+    }
 
-       ImageView image1 = (ImageView) findViewById(R.id.image_test1);
-       ImageView image2 = (ImageView) findViewById(R.id.image_test2);
-
-        Picasso.with(getApplicationContext())
-                .load("http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg")
-                .into(image1);
-        Picasso.with(getApplicationContext())
-                .load("http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg")
-                .into(image2);
-
-
+    @OnClick(R.id.showImagesGridExample)
+    public void showImagesGrid(View view) {
         startActivity(new Intent(getApplicationContext(), ImagesGridView.class));
     }
+
+
 }
